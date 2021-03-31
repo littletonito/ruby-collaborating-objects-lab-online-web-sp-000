255e2711b3fb15e0a4fff9a9f693404fd86d43a5
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-31 17:03:22 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -19,30 +19,6 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class MP3Importer
-#
-#   attr_reader :path
-#
-#   def initialize(path)
-#     @path = path
-#   end
-#
 #   def files
 #     @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
 #   end
